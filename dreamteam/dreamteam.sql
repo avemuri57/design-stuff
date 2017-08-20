06ad6b020f3947d8b184cf11e48f2f110b3e1f79
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-08-20 13:12:06 -0400

    more projects

---
+++ dreamteam/dreamteam.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE dreamteam_db;
+GRANT ALL PRIVILEGES ON dreamteam_db . * TO 'dt_admin'@'localhost';
+
---
+++ react/es6-counter/script.jsx
@@ -0,0 +1,14 @@
+class Counter extends React.Component{
+
+	render(){
+		return(
+			<h1> Built with ES6</>
+			)
+	}
+
+}
+
+ReactDOM.render(
+	<Counter/>,
+	document.getElementById('app')
+);
